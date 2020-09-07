ff7c8d8b22450c3d1b74b76cac5df8be3afc7b54
Masud Dipu <dipu.ngdc13@gmail.com>
2020-09-07 18:01:37 +0600

    final commit

---
+++ questions/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from questions.api import views as qv
+
+router = DefaultRouter()
+
+router.register(r"questions", qv.QuestionViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("questions/<slug:slug>/answer/", qv.AnswerCreateAPIView.as_view(), name="answer-create"),
+    path("questions/<slug:slug>/answers/", qv.AnswerListAPIView.as_view(), name="answer-list"),
+    path("answers/<int:pk>/", qv.AnswerRUDAPIVew.as_view(), name="answer-detail"),
+    path("answers/<int:pk>/like/", qv.AnswerLikeAPIView.as_view(), name="answer-like"),
+]
+
---
+++ questions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class QuestionsConfig(AppConfig):
+    name = 'questions'
+    def ready(self):
+        import questions.signals
